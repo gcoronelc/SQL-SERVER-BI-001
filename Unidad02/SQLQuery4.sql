be27a6100d150258a28a159c8234e6a40c99853d
Gustavo Coronel <gcoronelc@gmail.com>
2020-11-22 13:30:11 -0500

    Semana 02

---
+++ Unidad02/SQLQuery4.sql
@@ -0,0 +1,53 @@
+
+
+
+
+
+select * from [DatosRegiones];
+go
+
+create procedure dbo.uspClientes
+as
+begin
+select * from dbo.Customers;
+end;
+go
+
+exec dbo.uspClientes;
+go
+
+begin tran;
+go
+
+select @@TRANCOUNT;
+go
+
+
+delete from [Order Details];
+
+select * from [Order Details];
+go
+
+
+rollback;
+go
+
+
+begin tran;
+go
+
+select @@TRANCOUNT;
+go
+
+
+select * from dbo.PAGO;
+go
+
+truncate table dbo.pago;
+go
+
+rollback;
+go
+
+
+
