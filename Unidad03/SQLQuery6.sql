77bf81cf185e4f857e49ec95e12e9e39b2148bab
Gustavo Coronel <gcoronelc@gmail.com>
2020-11-29 12:37:25 -0500

    Unidad 04

--- Unidad03/SQLQuery6.sql
+++ Unidad03/SQLQuery6.sql
@@ -13,3 +13,55 @@ join Northwind.dbo.Categories c on p.CategoryID = c.CategoryID
 join Northwind.dbo.Suppliers s on p.SupplierID = s.SupplierID;
 go
 
+
+select * from dbo.CursoProgramado
+where IdProfesor is null;
+go
+
+select isnull(IdProfesor,'NONE'), count(1) cant_cursos
+from dbo.CursoProgramado
+where Activo = 1
+group by  IdProfesor;
+go
+
+
+select * from Northwind.dbo.Customers;
+go
+
+
+
+
+
+
+
+
+go
+
+
+
+select * from dbo.Ventas01;
+select * from dbo.Ventas02;
+select * from dbo.Ventas03;
+go
+
+truncate table dbo.Ventas01;
+truncate table dbo.Ventas02;
+truncate table dbo.Ventas03;
+go
+
+
+select * from dbo.Tabla1;
+select * from dbo.Tabla2;
+select * from dbo.Tabla3;
+go
+
+
+truncate table dbo.tabla1;
+truncate table dbo.tabla2;
+truncate table dbo.tabla3;
+go
+
+select * from dbo.DatosEmpleado;
+
+
+
